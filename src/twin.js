26bb9aae39deada74603b09d7148aa6275745f94
plasmashadow <plasmashadowx@gmail.com>
2015-09-05 23:57:31 +0530

    changes

---
+++ src/twin.js
@@ -0,0 +1,17 @@
+(function(exports){
+
+  var __maps = {
+    "get": "get_",
+    "post": "post_",
+    "put": "put_",
+    "delete": "delete_"
+  }
+
+  
+
+  function TwinRouter(){
+
+  }
+
+
+})(module.exports);
